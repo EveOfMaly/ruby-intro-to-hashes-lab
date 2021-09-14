0ec5d8b10356525c92affdc2562b0e3416a85184
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-09-14 13:43:12 -0700

    commit tree af61c8aad4e06fab8933099ae834f7308a01b5ae

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,78 +1,192 @@
+require 'pry'
+
 def new_hash
- Hash.new
+	Hash.new
 end
 
-def actor
+def actor 
 	{:name => "Dwayne The Rock Johnson"}
 end
 
-def monopoly
-	monopoly = {
+def monopoly 
+	{
 		:railroads => {}
 	}
-
 end
 
+
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	monopoly = {
-		:railroads => {
+	{
+		:railroads => 
+		{
 			:pieces => 4,
 			:names => {},
 			:rent_in_dollars => {}
 		}
 	}
-
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
-	monopoly = {
-		:railroads => {
+	{
+		:railroads => 
+		{
 			:pieces => 4,
-			:names => {
-				:reading_railroad => {},
-				:pennsylvania_railroad => {},
-				:b_and_o_railroad => {},
-				:shortline => {}
+			:names => 
+			{
+			:reading_railroad => {},
+			:pennsylvania_railroad => {},
+			:b_and_o_railroad => {},
+			:shortline => {}
 			},
-			:rent_in_dollars => {
+			:rent_in_dollars => 
+			{
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
 				:three_pieces_owned => 100,
 				:four_pieces_owned => 200
 			}
-		}
+		},
 	}
 end
+
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
-	monopoly = {
-		:railroads => {
+	{
+		:railroads => 
+		{
 			:pieces => 4,
-			:names => {
-				:reading_railroad => {
-					"mortgage_value" => '$100',
-				},
-				:pennsylvania_railroad => {
-					"mortgage_value" => '$200',
-				},
-				:b_and_o_railroad => {
-					"mortgage_value" => '$400',
-				},
-				:shortline => {
-					"mortgage_value" => '$800',
-				}
+			:names => 
+			{
+			:reading_railroad => 
+			{
+				"mortgage_value" => '$100'
 			},
-			:rent_in_dollars => {
+			:pennsylvania_railroad => 
+			{
+				"mortgage_value"=> '$200'
+			},
+			:b_and_o_railroad => 
+			{
+				"mortgage_value"=> '$400'
+			},
+			:shortline => 
+			{
+				"mortgage_value" => '$800'
+			}
+			},
+			:rent_in_dollars => 
+			{
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
 				:three_pieces_owned => 100,
 				:four_pieces_owned => 200
 			}
-		}
+		},
 	}
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def new_hash
+#  Hash.new
+# end
+
+# def actor
+# 	{:name => "Dwayne The Rock Johnson"}
+# end
+
+# def monopoly
+# 	monopoly = {
+# 		:railroads => {}
+# 	}
+
+# end
+
+# def monopoly_with_second_tier
+# 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+# 	monopoly = {
+# 		:railroads => {
+# 			:pieces => 4,
+# 			:names => {},
+# 			:rent_in_dollars => {}
+# 		}
+# 	}
+
+# end
+
+# def monopoly_with_third_tier
+# 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+
+# 	monopoly = {
+# 		:railroads => {
+# 			:pieces => 4,
+# 			:names => {
+# 				:reading_railroad => {},
+# 				:pennsylvania_railroad => {},
+# 				:b_and_o_railroad => {},
+# 				:shortline => {}
+# 			},
+# 			:rent_in_dollars => {
+# 				:one_piece_owned => 25,
+# 				:two_pieces_owned => 50,
+# 				:three_pieces_owned => 100,
+# 				:four_pieces_owned => 200
+# 			}
+# 		}
+# 	}
+# end
+
+# def monopoly_with_fourth_tier
+# 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+
+# 	monopoly = {
+# 		:railroads => {
+# 			:pieces => 4,
+# 			:names => {
+# 				:reading_railroad => {
+# 					"mortgage_value" => '$100',
+# 				},
+# 				:pennsylvania_railroad => {
+# 					"mortgage_value" => '$200',
+# 				},
+# 				:b_and_o_railroad => {
+# 					"mortgage_value" => '$400',
+# 				},
+# 				:shortline => {
+# 					"mortgage_value" => '$800',
+# 				}
+# 			},
+# 			:rent_in_dollars => {
+# 				:one_piece_owned => 25,
+# 				:two_pieces_owned => 50,
+# 				:three_pieces_owned => 100,
+# 				:four_pieces_owned => 200
+# 			}
+# 		}
+# 	}
+# 	binding.pry
+# end
+
+
+# monopoly_with_fourth_tier
